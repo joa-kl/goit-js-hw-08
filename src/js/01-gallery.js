a05d24e181fdcc64648b0925366ebd70c3b7734f
Joanna K <joannakleinrm@gmail.com>
2023-03-03 21:38:57 +0100

    simplebox link

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
