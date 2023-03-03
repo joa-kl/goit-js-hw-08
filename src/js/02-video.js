c752639e7d6ac8be5d6fff4be998482d8d73de33
Joanna K <joannakleinrm@gmail.com>
2023-03-03 23:03:40 +0100

    code added according to documentation

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+// import Player from '@vimeo/player';
+// const player = new Player('vimeo-player', {});
 
+// player.on('play', function () {
+//     console.log('played the video!');
+// });
+
+/* <script>
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function () {
+        console.log('played the video!');
+      });
+
+    player.getVideoTitle().then(function (title) {
+        console.log('title:', title);
+      });
+</script> */
+
+
+
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlayer = ({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on('timeupdate', throttle(onPlayer, 1000));
