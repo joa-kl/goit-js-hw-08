c0d3d85ba50941adc34d6f4757877785251e0e91
Joanna K <joannakleinrm@gmail.com>
2023-03-03 21:41:45 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,3 +7,32 @@ console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 // Dodatkowy import stylów
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const galleryItemsGrid = galleryItems
+    .map((image) => {
+        return `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`;
+    })
+    .join("");
+
+gallery.innerHTML = galleryItemsGrid;
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    function closeModal(event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
