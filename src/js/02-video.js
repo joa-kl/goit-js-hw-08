1c4736c3e51be38fd36af2a507d440b1d810273e
Joanna K <joannakleinrm@gmail.com>
2023-03-05 17:33:01 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,3 @@
-// import Player from '@vimeo/player';
-// const player = new Player('vimeo-player', {});
-
-// player.on('play', function () {
-//     console.log('played the video!');
-// });
-
-/* <script>
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function () {
-        console.log('played the video!');
-      });
-
-    player.getVideoTitle().then(function (title) {
-        console.log('title:', title);
-      });
-</script> */
-
-
-
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
